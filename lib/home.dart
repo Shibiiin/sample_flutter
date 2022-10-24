dfb881aab701bd112fc8a01a3edd5f9b6a2d8a63
Shibin <shibinm129@gmail.com>
2022-10-24 11:23:21 +0530

    commit

--- lib/home.dart
+++ lib/home.dart
@@ -1,17 +1,78 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child:
-        Text(
-          'Welcome',
-          style: TextStyle(
-              color: Colors.red, fontSize: 35, fontWeight: FontWeight.bold),
+    return Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(75),
+          child: AppBar(
+            automaticallyImplyLeading: false,
+            title: const Text(
+              "Login Page",
+              style: TextStyle(fontSize: 30),
+            ),
+            centerTitle: true,
+          ),
         ),
-      ),
-    );
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              /// Login Page Text
+              const Padding(
+                padding: EdgeInsets.only(top: 150),
+                child: Text(
+                  "LOGIN PAGE",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+                ),
+              ),
+
+              /// Textbox
+              const Padding(
+                padding: EdgeInsets.all(25.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      suffix: Icon(Icons.account_circle_outlined, size: 15),
+                      label: Text("Username"),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                      )),
+                ),
+              ),
+
+              ///Textbox
+              const SizedBox(
+                child: Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: TextField(
+                    obscuringCharacter: "*",
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        suffix: Icon(
+                          Icons.account_circle,
+                          size: 15,
+                        ),
+                        label: Text("Password"),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                        )),
+                  ),
+                ),
+              ),
+
+              ///Login button
+              Padding(
+                padding: const EdgeInsets.all(20.30),
+                child: ElevatedButton(onPressed: () {}, child: Text("login")),
+              ),
+
+              /// last user
+              TextButton(
+                  onPressed: () {}, child: Text("Not a User?,Register Here"))
+            ],
+          ),
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:sample_flutter/home.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -7,32 +10,46 @@ void main() {
   ));
 }
 
-class Splash extends StatelessWidget {
+class Splash extends StatefulWidget {
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 20), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>HomePage()));
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         decoration: const BoxDecoration(
-          image: DecorationImage(
-              fit: BoxFit.fill,
-              image: NetworkImage(
-                  "https://png.pngtree.com/thumb_back/fh260/back_pic/03/54/06/62579986dba62df.jpg"
-              ))
-        ),
+            image: DecorationImage(
+                fit: BoxFit.fill,
+                image: NetworkImage(
+                    "https://png.pngtree.com/thumb_back/fh260/back_pic/03/54/06/62579986dba62df.jpg"))),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Image.asset(
                 "assets/images/fluttericon.png",
+                color: Colors.blue,
+                alignment: Alignment.topRight,
                 height: 100,
                 width: 100,
               ),
+
               const Text("Welcome to Flutter",
+                  textAlign: TextAlign.left,
                   style: TextStyle(
-                      fontSize: 20,
+                      fontSize: 50,
                       fontWeight: FontWeight.bold,
-                      color: Colors.red)),
+                      color: Colors.white)),
             ],
           ),
         ),
