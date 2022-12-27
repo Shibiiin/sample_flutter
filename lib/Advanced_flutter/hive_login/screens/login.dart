ac7cebad9c4e930fb5108172c2f75b95f29464c7
Shibin <shibinm129@gmail.com>
2022-12-27 16:40:03 +0530

    commit

--- lib/Advanced_flutter/hive_login/screens/login.dart
+++ lib/Advanced_flutter/hive_login/screens/login.dart
@@ -1,4 +1,3 @@
-
 import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
@@ -13,6 +12,7 @@ import 'home_screen.dart';
 
 class LoginScreen extends StatelessWidget {
   LoginScreen({super.key});
+
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
 
@@ -21,119 +21,115 @@ class LoginScreen extends StatelessWidget {
     return Scaffold(
       body: Container(
           child: Stack(
-            children: [
-              Positioned(
-                top: 200,
-                left: 0,
-                right: 0,
-                child: Column(
-                  children: [
-                    BackdropFilter(
-                      filter: ImageFilter.blur(sigmaX: 2, sigmaY: 4),
-                      child: Container(
-                        height: 400,
-                        width: 350,
-                        padding: const EdgeInsets.all(20),
-                        decoration: BoxDecoration(
-                            color: Colors.grey[400],
-                            border: Border.all(
-                                color: Colors.black,
-                                width: 2),
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Column(
-                          children: [
-                            const SizedBox(height: 50),
-                            TextField(
-                              controller: _emailController,
-                              style: const TextStyle(
-                                color: Colors.black,
-                                fontSize: 20,
+        children: [
+          Positioned(
+            top: 200,
+            left: 0,
+            right: 0,
+            child: Column(
+              children: [
+                BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 2, sigmaY: 4),
+                  child: Container(
+                    height: 400,
+                    width: 350,
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                        color: Colors.grey[400],
+                        border: Border.all(color: Colors.black, width: 2),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 50),
+                        TextField(
+                          controller: _emailController,
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                          ),
+                          decoration: InputDecoration(
+                              hintText: 'Email',
+                              hintStyle: const TextStyle(
+                                  color: Colors.black, fontSize: 16),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20.0),
                               ),
-                              decoration:  InputDecoration(
-                                hintText: 'Email',
-                                hintStyle: const TextStyle(
-                                    color: Colors.black, fontSize: 16),
-                                enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(20.0),
-                                ),
-                                filled: true,
-                                fillColor: Colors.white
-                              ),
-                            ),
-                            const SizedBox(height: 20),
-                            TextField(
-                              obscureText: true,
-                              obscuringCharacter: '*',
-                              controller: _passwordController,
-                              style: const TextStyle(
-                                color: Colors.black,
-                                fontSize: 18,
-                              ),
-                              decoration:  InputDecoration(
-                                hintText: 'Password',
-                                hintStyle: const TextStyle(
-                                    color: Colors.black, fontSize: 16),
-                                enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(20.0),
-                                ),
-                                fillColor: Colors.white,
-                                filled: true,
-                              ),
-                            ),
-                            const SizedBox(height: 50),
-                            SizedBox(
-                              height: 40,
-                              width: 200,
-                              child: ElevatedButton(
-                                onPressed: () async {
-                                  final list =
-                                  await DBFunctions.instance.getUsers();
-                                  checkUser(list);
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor:
-                                  Colors.white.withOpacity(0.9),
-                                ),
-                                child: const Text(
-                                  'Login',
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ),
-                            ),
-                            const SizedBox(height: 20),
-                            TextButton(
-                                onPressed: () {
-                                  Get.to(
-                                        () => SignupScreen(),
-                                    transition: Transition.leftToRightWithFade,
-                                  );
-                                },
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: const [
-                                    Text(
-                                      'Dont have an account? ',
-                                      style: TextStyle(color: Colors.black),
-                                    ),
-                                    Text(
-                                      'Signup',
-                                      style: TextStyle(
-                                          color: Colors.blue,
-                                          fontWeight: FontWeight.bold),
-                                    )
-                                  ],
-                                ))
-                          ],
+                              filled: true,
+                              fillColor: Colors.white),
                         ),
-                      ),
+                        const SizedBox(height: 20),
+                        TextField(
+                          obscureText: true,
+                          obscuringCharacter: '*',
+                          controller: _passwordController,
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                          ),
+                          decoration: InputDecoration(
+                            hintText: 'Password',
+                            hintStyle: const TextStyle(
+                                color: Colors.black, fontSize: 16),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                            fillColor: Colors.white,
+                            filled: true,
+                          ),
+                        ),
+                        const SizedBox(height: 50),
+                        SizedBox(
+                          height: 40,
+                          width: 200,
+                          child: ElevatedButton(
+                            onPressed: () async {
+                              final list =
+                                  await DBFunctions.instance.getUsers();
+                              checkUser(list);
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.white.withOpacity(0.9),
+                            ),
+                            child: const Text(
+                              'Login',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        TextButton(
+                            onPressed: () {
+                              Get.to(
+                                () => SignupScreen(),
+                                transition: Transition.leftToRightWithFade,
+                              );
+                            },
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const [
+                                Text(
+                                  'Dont have an account? ',
+                                  style: TextStyle(color: Colors.black),
+                                ),
+                                Text(
+                                  'Signup',
+                                  style: TextStyle(
+                                      color: Colors.blue,
+                                      fontWeight: FontWeight.bold),
+                                )
+                              ],
+                            ))
+                      ],
                     ),
-                  ],
+                  ),
                 ),
-              )
-            ],
-          )),
+              ],
+            ),
+          )
+        ],
+      )),
     );
   }
 
@@ -151,7 +147,6 @@ class LoginScreen extends StatelessWidget {
         }
       });
       if (isUserFound == true) {
-
         Get.offAll(() => HomeScreen(email: email));
         Get.snackbar(
           'Success',
